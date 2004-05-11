d75bc1c75061a64039055334a9006f278d005983
dgt84 <dgt84@fd53868f-e4f1-0310-84ca-8663aff3ef64>
2004-05-11 16:24:04 +0000

    Updated with Chromakode's additions

--- trunk/utils/theme-utils/themeconvert/translate.py
+++ trunk/utils/theme-utils/themeconvert/translate.py
@@ -4,7 +4,7 @@
 	translate
 	Translate icon themes using an XML translation database
 
-	Copyright (C) 2004 Daniel G. Taylor
+	Copyright (C) 2004 Daniel G. Taylor and Chromakode
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -20,6 +20,9 @@
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
+
+import os, os.path
+import shutil
 
 def convert_xmldb_to_dict(xmldb, from_theme_type, to_theme_type):
 	"""
@@ -69,6 +72,31 @@ def convert_xmldb_to_dict(xmldb, from_theme_type, to_theme_type):
 	# return the dict
 	return translation_dict
 
+def copy_dir(src, dst, symlinks=False):
+	"""
+	Recursively copy a directory tree using copy2().
+
+	Based on shutil.copytree()
+
+	If the optional symlinks flag is true, symbolic links in the
+	source tree result in symbolic links in the destination tree; if
+	it is false, the contents of the files pointed to by symbolic
+	links are copied.
+	"""
+	names = os.listdir(src)
+	if not os.path.isdir(dst):
+		os.mkdir(dst)
+	for name in names:
+		srcname = os.path.join(src, name)
+		dstname = os.path.join(dst, name)
+		if symlinks and os.path.islink(srcname):
+			linkto = os.readlink(srcname)
+			os.symlink(linkto, dstname)
+		elif os.path.isdir(srcname):
+			copytree(srcname, dstname, symlinks)
+		else:
+			shutil.copy2(srcname, dstname)
+
 def translate_theme(from_base_path, to_base_path, translation_dict):
 	"""
 	Translate theme from from_base_path into to_base_path using
@@ -77,13 +105,50 @@ def translate_theme(from_base_path, to_base_path, translation_dict):
 	directory.icon would copy over to gnome-dev-directory.icon. The
 	same goes for PNG and SVG images.
 	"""
-	# here is the basic idea, although it needs to be done recursively:
+
+	# note: this code is python 2.3 dependent. porting should not be difficult,
+	# but python 2.3 makes copying and walking directories much easier.  
+
 	# for each dir in from_base_path
-		# if the dir name is in translation_dict.keys()
-			# translate all the icons using the dir names
-		# else
-			# for each file in the directory
-				# cut off the from_base_path and suffix:
-				# /base/devices/cdrom.svg -> devices/cdrom
-				# if value from above is in translation_dict.keys()
-					# translate icon name, add suffix and save
+	for root, dirs, files in os.walk(from_base_path):
+		# make root relative to from_base_path
+		# this is a hack, look for a better solution
+		# remove base from root
+		root = root.replace(from_base_path, "")
+		# remove any leading/trailing slashes, to make sure it's relative
+		root = root.strip("/")
+
+		# get the final component of root (the current directory name)
+		dirname = os.path.basename(root)
+		# if the dir name is in translation_dict
+		if translation_dict.has_key(dirname):
+			# translate all the icons using the dir name
+			# get destination path in to_base_path
+			for dest in translation_dict[dirname]:
+				to_dest_path = os.path.join(to_base_path, dest)
+				# recreate directories between the root of to_base_path and the destination
+				try:
+					os.makedirs(to_dest_path)
+				except: pass
+				# copy
+				copy_dir(root, to_dest_path)
+
+		for file_ in files:
+			# separate filename and ext
+			file_, ext = os.path.splitext(file_)
+			# add the current directory, in the system translation_dict uses
+			translation_name = os.path.join(dirname,file_)
+			# if value from above is in translation_dict
+			if translation_dict.has_key(translation_name):
+				# translate icon name, add suffix and save
+				for dest in translation_dict[translation_name]:
+					print os.path.join(root, file_+ext), os.path.join(to_base_path, dest+ext)
+					to_dest_path = os.path.join(to_base_path, dest+ext)
+					# recreate directories between the root of to_base_path and the destination
+					try:
+						os.makedirs(os.path.dirname(to_dest_path))
+					except OSError, e:
+						# why is this happening when the path exists???
+						print e
+					# copy
+					shutil.copy2(os.path.join(root, file_+ext), to_dest_path)
