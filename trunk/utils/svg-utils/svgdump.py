755a295de563460a01e9b00788b3a792270da0ea
dgt84 <dgt84@fd53868f-e4f1-0310-84ca-8663aff3ef64>
2004-06-13 15:35:51 +0000

    Major code rewrite to use the new svg classes

--- trunk/utils/svg-utils/svgdump.py
+++ trunk/utils/svg-utils/svgdump.py
@@ -21,64 +21,38 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
 
-from svgfile import SVGFile
+from svg.svgfile import SVGFile
 
-def print_gradients(gradients, gradient_type):
+def print_info(element, tabs = ''):
 	"""
-	Print out the gradients and stops returned by
-	SVGFile.get_linear_gradients or SVGFile.get_radial_gradients
+	Print information about an SVG element
 	"""
-	for gradient in gradients:
-		stops = gradient.get_stops()
-		if stops:
-			print gradient_type + " Gradient '" + gradient.get_name() + "'"
-			for stop in stops:
-				print "\tStop '" + stop.get_name() + "': " + stop.get_color()
-		else:
-			xlink = gradient.get_xlink()
-			if xlink:
-				print gradient_type + " Gradient '" + gradient.get_name() + "'"
-				print "\tXLink: " + xlink
-
-def print_object_data(objects, object_type):
-	"""
-	Print out the objects' fill and stroke colors
-	objects should be a list of Rects, Paths, or Polygons
-	"""
-	for object in objects:
-		print object_type + " '" + object.get_name() + "':"
-		fill = object.get_fill_color()
-		stroke = object.get_stroke_color()
-		if fill:
-			if fill[:3] == "url":
-				print "\tFill Link: " + fill[4:-3]
-				print "\tFill Opacity: " + fill[-2:]
-			else:
-				print "\tFill Color: " + fill
-		if stroke:
-			print "\tStroke Color: " + stroke
-
-def print_svg_color_data(linear_gradients, radial_gradients, rects, \
-						circles, ellipses, lines, polylines, polygons, \
-						paths, texts, tspans, trefs, text_paths, groups):
-	"""
-	Print out all the color data from an svg image nicely
-	"""
-	# print out the data
-	print_gradients(linear_gradients, "Linear")
-	print_gradients(radial_gradients, "Radial")
-	print_object_data(rects, "Rect")
-	print_object_data(circles, "Circle")
-	print_object_data(ellipses, "Ellipse")
-	print_object_data(lines, "Line")
-	print_object_data(polylines, "Polyline")
-	print_object_data(polygons, "Polygon")
-	print_object_data(paths, "Path")
-	print_object_data(texts, "Text")
-	print_object_data(tspans, "TSpan")
-	print_object_data(trefs, "TRef")
-	print_object_data(text_paths, "Text Path")
-	print_object_data(groups, "Group")
+	# get any containers
+	for container in element.containers:
+		if container.tag == "defs": tag = "Defs"
+		elif container.tag == "g": tag = "Group"
+		print tabs + tag
+		print tabs + "  Name: " + container.id
+		print_info(container, tabs + "  ")
+	# get the gradients
+	for grad in element.gradients:
+		print tabs + grad.tag
+		print tabs + "  Name: " + grad.id
+		for stop in grad.stops:
+			print tabs + "  Stop"
+			print tabs + "    Name: " + stop.id
+			print tabs + "    Color: " + stop.color
+			print tabs + "    Offset: " + stop.offset
+		if grad.has_attribute("xlink"):
+			print tabs + "  xlink: " + grad.xlink
+	# get the shapes
+	for shape in element.shapes:
+		print tabs + shape.tag
+		print tabs + "  Name: " + shape.id
+		if shape.has_attribute("fill"):
+			print tabs + "  Fill: " + shape.fill
+		if shape.has_attribute("stroke"):
+			print tabs + "  Stroke: " + shape.stroke
 
 if __name__ == "__main__":
 	from sys import argv, exit
@@ -90,8 +64,6 @@ if __name__ == "__main__":
 		print "Copyright 2004 Daniel G. Taylor, released under the GNU GPL"
 		print "Usage:"
 		print "svgdump file.svg"
-		print "svgdump /path/to/search/for/svgs"
-		print "Note that the SVG files must have an svg extension for them to be included!"
 		exit()
 
 	path = argv[1]
@@ -100,143 +72,9 @@ if __name__ == "__main__":
 		print "That path does not exist!"
 		exit()
 
-	if os.path.isfile(path):
-		# single file, so let's print out some info
-		svg = SVGFile(path)
-		print "Name: " + svg.get_name()
-		print "Width: " + svg.get_width()
-		print "Height: " + svg.get_height()
-		ink_ver = svg.get_inkscape_version()
-		sod_ver = svg.get_sodipodi_version()
-		xmlns = svg.get_xmlns()
-		xmlns_ink = svg.get_xmlns_inkscape()
-		xmlns_sod = svg.get_xmlns_sodipodi()
-		xmlns_x = svg.get_xmlns_xlink()
-		if ink_ver:
-			print "Inkscape Version: " + ink_ver
-		if sod_ver:
-			print "Sodipodi Version: " + sod_ver
-		if xmlns:
-			print "XML Namespace: " + xmlns
-		if xmlns_ink:
-			print "Inkscape Namespace: " + xmlns_ink
-		if xmlns_sod:
-			print "Sodipodi Namespace: " + xmlns_sod
-		if xmlns_x:
-			print "Xlink Namespace: " + xmlns_x
+	svg = SVGFile(path)
 
-		# get the image's info
-		linear_gradients = svg.get_linear_gradients()
-		radial_gradients = svg.get_radial_gradients()
-		rects = svg.get_rects()
-		circles = svg.get_circles()
-		ellipses = svg.get_ellipses()
-		lines = svg.get_lines()
-		polylines = svg.get_polylines()
-		polygons = svg.get_polygons()
-		paths = svg.get_paths()
-		texts = svg.get_texts()
-		tspans = svg.get_tspans()
-		trefs = svg.get_trefs()
-		text_paths = svg.get_text_paths()
-		groups = svg.get_groups()
-
-		# print out the color data
-		print_svg_color_data(linear_gradients, radial_gradients, rects, \
-							circles, ellipses, lines, polylines, polygons, \
-							paths, texts, tspans, trefs, text_paths, groups)
-
-	elif os.path.isdir(path):
-		# multiple files, let's get all the colors
-		def crawl_dir(path, lg, rg, r, c, e, l, pl, pg, pa, te, ts, tr, tp, g):
-			"""
-			recursively crawl a directory and get all the used
-			gradients and colors of the SVG files within
-			"""
-			print "Scanning " + path
-			# for each file or dir (let's just call it a node)
-			for node in os.listdir(path):
-				abspath = os.path.join(path, node)
-				if os.path.isfile(abspath) and node[-3:].lower() == "svg":
-					# parse the file
-					svg = SVGFile(abspath)
-					# let's get the file's data
-					linear_gradients = svg.get_linear_gradients()
-					radial_gradients = svg.get_radial_gradients()
-					rects = svg.get_rects()
-					circles = svg.get_circles()
-					ellipses = svg.get_ellipses()
-					lines = svg.get_lines()
-					polylines = svg.get_polylines()
-					polygons = svg.get_polygons()
-					paths = svg.get_paths()
-					texts = svg.get_texts()
-					tspans = svg.get_tspans()
-					trefs = svg.get_trefs()
-					text_paths = svg.get_text_paths()
-					groups = svg.get_groups()
-					# add unique values to the passed containers
-					for gradient in linear:
-						color = []
-						for stop in gradient.get_stops():
-							color.append(stop.get_color())
-						if color not in lg:
-							lg.append(color)
-					for gradient in radial:
-						color = []
-						for stop in gradient.get_stops():
-							color.append(stop.get_color())
-						if color not in rg:
-							rg.append(color)
-					for objs,ls in [(rects, r), (circles, c), (ellipses, e), (lines, l), (polylines, pl), (polygons, pg), (paths, pa), (texts, te), (tspans, ts), (trefs, tr), (text_paths, tp), (groups, g)]:
-						for obj in objs:
-							fcolor = obj.get_fill_color()
-							scolor = obj.get_stroke_color()
-							# we don't care about url() links right now
-							if fcolor:
-								fcolor.strip()
-								if fcolor[:3] == "url":
-									fcolor = None
-							if scolor:
-								scolor.strip()
-								if scolor[:3] == "url":
-									scolor = None
-							if (fcolor, scolor) not in ls:
-								ls.append((fcolor, scolor))
-
-				elif os.path.isdir(abspath):
-					# oh, fun! recursion!
-					crawl_dir(abspath, lg, rg, r, c, e, l, pl, pg, pa, te, ts, tr, tp, g)
-
-		def print_multi(lg, rg, r, c, e, l, pl, pg, pa, te, ts, tr, tp, g):
-			"""
-			Print out the info from multiple files
-			This only prints out the colors from the lists passed to it
-			"""
-			for gradient in lg:
-				if len(gradient):
-					print "Linear Gradient:"
-				for color in gradient:
-					print "\tStop: " + color
-			for gradient in rg:
-				if len(gradient):
-					print "Radial Gradient:"
-				for color in gradient:
-					print "\tStop: " + color
-			for objs,name in [(r, "Rect"), (c, "Cirlce"), (e, "Elipse"), (l, "Line"), (pl, "Polyline"), (pg, "Polygon"), (pa, "Path"), (te, "Text"), (ts, "TSpan"), (tr, "TRef"), (tp, "Text Path"), (g, "Group")]:
-				for obj in objs:
-					fill, stroke = obj
-					if fill or stroke:
-						print name + ":"
-					if fill:
-						print "\tFill: " + fill
-					if stroke:
-						print "\tStroke: " + stroke
-
-		# =====================================================================
-		print "Multi-file mode activated, printing all found gradients and colors:"
-		lg, rg, r, c, e, l, pl, pg, pa, te, ts, tr, tp, g = \
-		([], [], [], [], [], [], [], [], [], [], [], [], [], [])
-		crawl_dir(path, lg, rg, r, c, e, l, pl, pg, pa, te, ts, tr, tp, g)
-		# print out all the data
-		print_multi(lg, rg, r, c, e, l, pl, pg, pa, te, ts, tr, tp, g)
+	print "Name: " + svg.id
+	print "Width: " + svg.width
+	print "Height: " + svg.height
+	print_info(svg)
