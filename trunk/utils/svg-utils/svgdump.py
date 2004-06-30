e178afa1352ff96831f32ea9f5f8f63f90c73625
dgt84 <dgt84@fd53868f-e4f1-0310-84ca-8663aff3ef64>
2004-06-30 16:13:36 +0000

    Use shortcut to import SVGFile

--- trunk/utils/svg-utils/svgdump.py
+++ trunk/utils/svg-utils/svgdump.py
@@ -21,7 +21,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
 
-from svg.svgfile import SVGFile
+from svg import SVGFile
 
 def print_info(element, tabs = ''):
 	"""
