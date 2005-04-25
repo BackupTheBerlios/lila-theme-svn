c13f7b552b673b8461fc456e98c420369c49f89d
jpetso <jpetso@fd53868f-e4f1-0310-84ca-8663aff3ef64>
2005-04-25 21:16:11 +0000

    one last time. really. I won't need to fix makerelease.sh anymore.

--- trunk/lila/kde/makerelease.sh
+++ trunk/lila/kde/makerelease.sh
@@ -34,8 +34,8 @@ echo "Cleaning up previous release folder..."
 rm -rf $OUTPUTPATH
 mkdir -p $OUTPUTPATH/lila-kde
 
-echo "Copying this directory's contents to $OUTPUTPATH/lila-kde/lila..."
-cp -r . $OUTPUTPATH/lila-kde/lila
+echo "Copying this directory's contents to $OUTPUTPATH/lila-kde/lila-kde..."
+cp -r . $OUTPUTPATH/lila-kde/lila-kde
 
 
 # Part 2: make color mods
@@ -43,29 +43,29 @@ cp -r . $OUTPUTPATH/lila-kde/lila
 echo
 echo "Making color mods..."
 for COLOR in $COLORMODS; do
-	mkdir $OUTPUTPATH/lila-kde/lila-$COLOR
-	./makecolormod.sh $COLOR $OUTPUTPATH/lila-kde/lila $OUTPUTPATH/lila-kde/lila-$COLOR
+	mkdir $OUTPUTPATH/lila-kde/lila-kde-$COLOR
+	./makecolormod.sh $COLOR $OUTPUTPATH/lila-kde/lila-kde $OUTPUTPATH/lila-kde/lila-kde-$COLOR
 done
 
 
 # Part 3: generate PNG files from the SVG sources
 
 echo
-echo "Generating PNG files in $OUTPUTPATH/lila-kde/lila..."
+echo "Generating PNG files in $OUTPUTPATH/lila-kde/lila-kde..."
 echo
-sh -c "cd $OUTPUTPATH/lila-kde/lila && ./generate.py --noinstall"
+sh -c "cd $OUTPUTPATH/lila-kde/lila-kde && ./generate.py --noinstall"
 for COLOR in $COLORMODS; do
 	echo
-	echo "Generating PNG files in $OUTPUTPATH/lila-kde/lila-$COLOR..."
+	echo "Generating PNG files in $OUTPUTPATH/lila-kde/lila-kde-$COLOR..."
 	echo
 
 	# non-creatable files
-	cp generate.py $OUTPUTPATH/lila-kde/lila-$COLOR/
-	cp *_connecting.mng $OUTPUTPATH/lila-kde/lila-$COLOR/
-	cp ChangeLog COPYRIGHT LICENSE README TODO $OUTPUTPATH/lila-kde/lila-$COLOR/
-	cp index-$COLOR.desktop $OUTPUTPATH/lila-kde/lila-$COLOR/index.desktop
+	cp generate.py $OUTPUTPATH/lila-kde/lila-kde-$COLOR/
+	cp *_connecting.mng $OUTPUTPATH/lila-kde/lila-kde-$COLOR/
+	cp ChangeLog COPYRIGHT LICENSE README TODO FAQ $OUTPUTPATH/lila-kde/lila-kde-$COLOR/
+	cp index-$COLOR.desktop $OUTPUTPATH/lila-kde/lila-kde-$COLOR/index.desktop
 
-	sh -c "cd $OUTPUTPATH/lila-kde/lila-$COLOR && ./generate.py --noinstall"
+	sh -c "cd $OUTPUTPATH/lila-kde/lila-kde-$COLOR && ./generate.py --noinstall"
 done
 
 
@@ -73,24 +73,25 @@ done
 
 echo
 echo "Removing unnecessary files and directories..."
-rm -rf $OUTPUTPATH/lila-kde/lila/.svn #> /dev/null
-rm -rf $OUTPUTPATH/lila-kde/lila/*/.svn #> /dev/null
-rm -rf $OUTPUTPATH/lila-kde/lila/*/*/.svn #> /dev/null
-rm -rf $OUTPUTPATH/lila-kde/lila/ksplash #> /dev/null
-rm -rf $OUTPUTPATH/lila-kde/lila/scalable #> /dev/null
-rm -f $OUTPUTPATH/lila-kde/lila/*_connecting.mng #> /dev/null
-rm -f $OUTPUTPATH/lila-kde/lila/generate.py #> /dev/null
-rm -f $OUTPUTPATH/lila-kde/lila/makerelease.sh #> /dev/null
-rm -f $OUTPUTPATH/lila-kde/lila/makecolormod.sh #> /dev/null
-rm -f $OUTPUTPATH/lila-kde/lila/convert-errors.log #> /dev/null
-rm -f $OUTPUTPATH/lila-kde/lila/index-scalable.desktop #> /dev/null
+rm -rf $OUTPUTPATH/lila-kde/lila-kde/.svn #> /dev/null
+rm -rf $OUTPUTPATH/lila-kde/lila-kde/*/.svn #> /dev/null
+rm -rf $OUTPUTPATH/lila-kde/lila-kde/*/*/.svn #> /dev/null
+rm -rf $OUTPUTPATH/lila-kde/lila-kde/ksplash #> /dev/null
+rm -rf $OUTPUTPATH/lila-kde/lila-kde/scalable #> /dev/null
+rm -f $OUTPUTPATH/lila-kde/lila-kde/*_connecting.mng #> /dev/null
+rm -f $OUTPUTPATH/lila-kde/lila-kde/generate.py #> /dev/null
+rm -f $OUTPUTPATH/lila-kde/lila-kde/makerelease.sh #> /dev/null
+rm -f $OUTPUTPATH/lila-kde/lila-kde/makecolormod.sh #> /dev/null
+rm -f $OUTPUTPATH/lila-kde/lila-kde/convert-errors.log #> /dev/null
+rm -f $OUTPUTPATH/lila-kde/lila-kde/index-scalable.desktop #> /dev/null
 
 for COLOR in $COLORMODS; do
-	rm -rf $OUTPUTPATH/lila-kde/lila-$COLOR/ksplash #> /dev/null
-	rm -rf $OUTPUTPATH/lila-kde/lila-$COLOR/scalable #> /dev/null
-	rm -f $OUTPUTPATH/lila-kde/lila-$COLOR/generate.py #> /dev/null
-	rm -f $OUTPUTPATH/lila-kde/lila-$COLOR/convert-errors.log #> /dev/null
-	rm -f $OUTPUTPATH/lila-kde/lila/index-$COLOR.desktop #> /dev/null
+	rm -rf $OUTPUTPATH/lila-kde/lila-kde-$COLOR/ksplash #> /dev/null
+	rm -rf $OUTPUTPATH/lila-kde/lila-kde-$COLOR/scalable #> /dev/null
+	rm -f $OUTPUTPATH/lila-kde/lila-kde-$COLOR/generate.py #> /dev/null
+	rm -f $OUTPUTPATH/lila-kde/lila-kde-$COLOR/convert-errors.log #> /dev/null
+	rm -f $OUTPUTPATH/lila-kde/lila-kde-$COLOR/*_connecting.mng #> /dev/null
+	rm -f $OUTPUTPATH/lila-kde/lila-kde/index-$COLOR.desktop #> /dev/null
 done
 
 
@@ -98,10 +99,10 @@ done
 
 echo
 echo "Making tarballs ($OUTPUTPATH/lila-kde[-color].tar.bz2)..."
-sh -c "cd $OUTPUTPATH/lila-kde && tar --create --bzip2 --file lila-kde.tar.bz2 lila"
+sh -c "cd $OUTPUTPATH/lila-kde && tar --create --bzip2 --file lila-kde.tar.bz2 lila-kde"
 
 for COLOR in $COLORMODS; do
-	sh -c "cd $OUTPUTPATH/lila-kde && tar --create --bzip2 --file lila-kde-$COLOR.tar.bz2 lila-$COLOR"
+	sh -c "cd $OUTPUTPATH/lila-kde && tar --create --bzip2 --file lila-kde-$COLOR.tar.bz2 lila-kde-$COLOR"
 done
 
 echo
